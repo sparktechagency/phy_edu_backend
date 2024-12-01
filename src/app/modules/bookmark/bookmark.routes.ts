f42ad17f13fb3eea7e48328bacf0e4d629e36fdf
manik <maniksarker652@gmail.com>
2024-12-01 14:54:38 +0600

    set bookmark routes

---
+++ src/app/modules/bookmark/bookmark.routes.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
+import BookmarkController from './bookmark.controller';
+
+const router = express.Router();
+
+router.post(
+  '/add-delete-article-bookmark/:id',
+  auth(USER_ROLE.user),
+  BookmarkController.articleBookmarkAddDelete,
+);
+router.get(
+  '/add-delete-video-bookmark/:id',
+  auth(USER_ROLE.user),
+  BookmarkController.videoBookmarkAddDelete,
+);
+
+router.get(
+  '/get-article-bookmark',
+  auth(USER_ROLE.user),
+  BookmarkController.getAllArticleBookmark,
+);
+router.get(
+  '/get-video-bookmark',
+  auth(USER_ROLE.user),
+  BookmarkController.getAllVideoBookmark,
+);
+
+export const bookmarkRoutes = router;
